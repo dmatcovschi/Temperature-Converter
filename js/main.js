0ddae5b26e85943e8106632739726c40a9ee99b9
dmatcovschi <dmatcovschi@gmail.com>
2015-05-09 23:57:55 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -51,3 +51,28 @@ function HeaderController($scope, $location)
         return viewLocation === $location.path();
     };
 }
+
+app.controller('searchCompany',function($scope){
+	$scope.query = {}
+	$scope.queryBy = '$'
+	$scope.companies = [
+		{
+			"name" : "Google",
+			"description" : "Since the beginning, we’ve focused on providing the best user experience possible. Whether we’re designing a new Internet browser or a new tweak to the look of the homepage, we take great care to ensure that they will ultimately serve you, rather than our own internal goal or bottom line.",
+			"link" : "http://www.artificialbrains.com/images/google/google-building-44.jpg",
+			"facebook" : "https://www.facebook.com/Google",
+			"linkedin" : "https://www.linkedin.com/company/google",
+			"twitter" : "https://twitter.com/google",
+			"googleplus" : "https://plus.google.com/+google/posts"
+		},
+		{
+			"name" : "Deloitte",
+			"description" : "Deloitte is the brand under which tens of thousands of dedicated professionals in independent firms throughout the world collaborate to provide audit, consulting, financial advisory, risk management, tax and related services to select clients.",
+			"link" : "http://farm4.staticflickr.com/3417/3369025249_62387e7d1e_z.jpg?zz=1",
+			"facebook" : "https://www.facebook.com/deloitte",
+			"linkedin" : "https://www.linkedin.com/company/deloitte",
+			"twitter" : "https://twitter.com/deloitte",
+			"googleplus" : "https://plus.google.com/+Deloitte/posts"
+		}
+	];
+});
