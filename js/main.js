5175cd8ced5cb2aee8096e1c7036e818ab448ce2
dmatcovschi <dmatcovschi@gmail.com>
2015-05-11 00:07:53 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -72,6 +72,42 @@ app.controller("searchCompany", function($scope){
 			"linkedin" : "https://www.linkedin.com/company/deloitte",
 			"twitter" : "https://twitter.com/deloitte",
 			"googleplus" : "https://plus.google.com/+Deloitte/posts"
+		},
+		{
+			"name" : "Symantec",
+			"description" : "Symantec helps consumers and organizations secure and manage their information-driven world. Learn about the company, read what media and analysts have to say about Symantec, or find positions open in our team.",
+			"link" : "http://cdn5.triplepundit.com/wp-content/uploads/2013/05/Symantec-Headquarters.jpg",
+			"facebook" : "https://www.facebook.com/Symantec",
+			"linkedin" : "https://www.linkedin.com/company/symantec",
+			"twitter" : "https://twitter.com/symantec",
+			"googleplus" : "https://plus.google.com/+symantec/posts"
+		},
+		{
+			"name" : "Amazon",
+			"description" : "With a mission to be Earth’s most customer-centric company, where customers can find and discover anything they might want to buy online, and endeavors to offer its customers the lowest possible prices, Amazon.com and other sellers offer millions of unique new, refurbished, and used items in categories such as books; movies; music & games; digital downloads; electronics & computers.",
+			"link" : "http://a.abcnews.com/images/Business/AP_amazon_kab_140425_16x9_608.jpg",
+			"facebook" : "https://www.facebook.com/Amazon",
+			"linkedin" : "https://www.linkedin.com/company/amazon",
+			"twitter" : "https://twitter.com/amazon",
+			"googleplus" : "https://plus.google.com/+amazon/posts"
+		},
+		{
+			"name" : "VMWare",
+			"description" : "The pace of business is melting away rigid structures and requires a new model of IT—one that is fluid, instant and secure. VMware offers that model—Brave New IT, empowering customers to rapidly develop, automatically deliver and safely consume all applications, from traditional to cloud-native, on any device and to manage them through a unified platform.",
+			"link" : "http://cdn.arstechnica.net/wp-content/uploads/2011/11/vmware_headquarters-4eb4083-intro.jpg",
+			"facebook" : "https://www.facebook.com/vmware",
+			"linkedin" : "https://www.linkedin.com/company/vmware",
+			"twitter" : "https://twitter.com/VMware?",
+			"googleplus" : "https://plus.google.com/+vmware/posts"
+		},
+		{
+			"name" : "Oracle",
+			"description" : "At Oracle, we want to help you shift that spending balance. We help you simplify your IT environment so that you can free up money, time, and resources to invest in innovation. We do this by providing hardware and software that’s engineered to work together, in the cloud or on premises.",
+			"link" : "http://news.doddleme.com/wp-content/uploads/2012/09/oracle-building.jpg",
+			"facebook" : "https://www.facebook.com/Oracle",
+			"linkedin" : "https://www.linkedin.com/company/oracle",
+			"twitter" : "https://twitter.com/oracle",
+			"googleplus" : "https://plus.google.com/+Oracle/posts"
 		}
 	];
 });
