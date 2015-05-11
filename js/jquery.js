42dd60b0d55940f4f34942139c101290067e7997
dmatcovschi <dmatcovschi@gmail.com>
2015-05-11 01:01:05 -0700

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,4 @@
-
+//change window replace to correct url
 $('#recruiters').click(function(){
   window.location.replace("http://dmatcovschi.github.io/#/recruiters")
   $('html, body').animate({scrollTop:$(document).height()}, 'slow');
