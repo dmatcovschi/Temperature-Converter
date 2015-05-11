170f3990fd2c27a48081dc21c848d2f3dfaf5722
dmatcovschi <dmatcovschi@gmail.com>
2015-05-11 01:00:17 -0700

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,6 @@
 
 $('#recruiters').click(function(){
+  window.location.replace("http://dmatcovschi.github.io/#/recruiters")
   $('html, body').animate({scrollTop:$(document).height()}, 'slow');
     return false;
 });
