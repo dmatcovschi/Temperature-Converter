19807a76bf9e9106dca6ae1a9bf48b656b3d3df3
dmatcovschi <dmatcovschi@gmail.com>
2015-05-12 01:19:09 -0700

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,18 +1,14 @@
 //change window replace to correct url
 $('#recruiters').click(function(){
   window.location.replace("http://dmatcovschi.github.io/#/recruiters")
-  $('html, body').animate({scrollTop:$(document).height()}, 'slow');
-    return false;
+  $('html, body').animate({scrollTop:$("recruiters").ofsset().top
+  },2000);
 });
 
 $('#recruiters1').click(function(){
   window.location.replace("http://dmatcovschi.github.io/#/recruiters")
   $('html, body').animate({scrollTop:$(document).height()}, 'slow');
     return false;
-});
-
-$('#feedbackform').click(function(){
-  window.location.replace("http://dmatcovschi.github.io/#/feedbackdisplay2")
 });
 
  // Activates the Carousel
