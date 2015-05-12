97cf2f7fd7c766ef581d102a95e77e122cf22c0a
dmatcovschi <dmatcovschi@gmail.com>
2015-05-11 18:29:57 -0700

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -5,6 +5,12 @@ $('#recruiters').click(function(){
     return false;
 });
 
+$('#recruiters1').click(function(){
+  window.location.replace("http://dmatcovschi.github.io/#/recruiters")
+  $('html, body').animate({scrollTop:$(document).height()}, 'slow');
+    return false;
+});
+
 $('#feedbackform').click(function(){
   window.location.replace("http://dmatcovschi.github.io/#/feedbackdisplay2")
 });
