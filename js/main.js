13957b271dcdd28c8c675fec0472346fb3a028c1
dmatcovschi <dmatcovschi@gmail.com>
2015-05-12 01:47:35 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -243,42 +243,18 @@ function Ctrl($scope)
     $scope.date = new Date();
 }
 
-app.controller("mainController",
-
-function ($scope) {
-
-    // Item List Arrays
-    $scope.items = [];
-    $scope.checked = [];
-
-    // Add a Item to the list
-    $scope.addItem = function () {
-
-        $scope.items.push({
-            amount: $scope.itemAmount,
-            name: $scope.itemName
-        });
-
-        // Clear input fields after push
-        $scope.itemAmount = "";
-        $scope.itemName = "";
-
-    };
-
-    // Add Item to Checked List and delete from Unchecked List
-    $scope.toggleChecked = function (index) {
-        $scope.checked.push($scope.items[index]);
-        $scope.items.splice(index, 1);
-    };
-
-    // Get Total Items
-    $scope.getTotalItems = function () {
-        return $scope.items.length;
-    };
-
-    // Get Total Checked Items
-    $scope.getTotalCheckedItems = function () {
-        return $scope.checked.length;
-    };
+app.controller('fessCntrl', function ($scope) {
+    $scope.expenses = [{
+  exTitle: "",
+  amount: "",
+  typeOfShare: "",
+  date: ""
+ }];
+ $scope.submitExpense = function (expenseInfo) {
+    $scope.expenses.push(expenseInfo);
+ }
+    
 });
+
+app.$inject = ['$scope'];
 
