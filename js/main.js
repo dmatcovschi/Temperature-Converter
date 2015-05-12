fb03d76a8d348180d4ac2c5fae3a85c51c19797b
davidwat <omegasereon@gmail.com>
2015-05-11 19:16:49 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/people", {templateUrl: "partials/people.html", controller: "PageCtrl"})
     .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
     .when("/feedbacklogin", {templateUrl: "partials/feedbacklogin.html", controller: "PageCtrl"})
+    .when("/feedbackdisplay2", {templateUrl: "partials/feedbackdisplay2.php", controller: "PageCtrl"})
     
     
     // else 404
@@ -57,7 +58,7 @@ app.controller("searchCompany", function($scope){
 		{
 			"name" : "KPMG",
 			"description" : "KPMG is one of the largest professional services companies in the world and one of the Big Four auditors, along with Deloitte, EY and PwC. Its global headquarters are located in Amsterdam, the Netherlands. KPMG employs 162,000 people and has three lines of services: audit, tax, and advisory. Its tax and advisory services are further divided into various service groups.",
-			"link" : "http://www.nrc.nl/wp-content/uploads/2013/11/kpmg-logo.jpg",
+			"link" : "http://www.nrc.nl/wp-content/uploads/2013/11/kpmg-logo.jpg" class="thumbnail" height="100" width="100",
 			"facebook" : "https://www.facebook.com/KPMG",
 			"linkedin" : "https://www.linkedin.com/company/kpmg",
 			"twitter" : "https://twitter.com/kpmg",
@@ -66,7 +67,7 @@ app.controller("searchCompany", function($scope){
 		{
 			"name" : "Deloitte",
 			"description" : "Deloitte is the brand under which tens of thousands of dedicated professionals in independent firms throughout the world collaborate to provide audit, consulting, financial advisory, risk management, tax and related services to select clients.",
-			"link" : "http://farm4.staticflickr.com/3417/3369025249_62387e7d1e_z.jpg?zz=1",
+			"link" : "http://farm4.staticflickr.com/3417/3369025249_62387e7d1e_z.jpg?zz=1" class="thumbnail" height="100" width="100",
 			"facebook" : "https://www.facebook.com/deloitte",
 			"linkedin" : "https://www.linkedin.com/company/deloitte",
 			"twitter" : "https://twitter.com/deloitte",
@@ -75,7 +76,7 @@ app.controller("searchCompany", function($scope){
 		{
 			"name" : "Symantec",
 			"description" : "Symantec helps consumers and organizations secure and manage their information-driven world. Learn about the company, read what media and analysts have to say about Symantec, or find positions open in our team.",
-			"link" : "http://cdn5.triplepundit.com/wp-content/uploads/2013/05/Symantec-Headquarters.jpg",
+			"link" : "http://cdn5.triplepundit.com/wp-content/uploads/2013/05/Symantec-Headquarters.jpg" class="thumbnail" height="100" width="100",
 			"facebook" : "https://www.facebook.com/Symantec",
 			"linkedin" : "https://www.linkedin.com/company/symantec",
 			"twitter" : "https://twitter.com/symantec",
@@ -84,7 +85,7 @@ app.controller("searchCompany", function($scope){
 		{
 			"name" : "Amazon",
 			"description" : "With a mission to be Earth’s most customer-centric company, where customers can find and discover anything they might want to buy online, and endeavors to offer its customers the lowest possible prices, Amazon.com and other sellers offer millions of unique new, refurbished, and used items in categories such as books; movies; music & games; digital downloads; electronics & computers.",
-			"link" : "http://a.abcnews.com/images/Business/AP_amazon_kab_140425_16x9_608.jpg",
+			"link" : "http://a.abcnews.com/images/Business/AP_amazon_kab_140425_16x9_608.jpg" class="thumbnail" height="100" width="100",
 			"facebook" : "https://www.facebook.com/Amazon",
 			"linkedin" : "https://www.linkedin.com/company/amazon",
 			"twitter" : "https://twitter.com/amazon",
@@ -93,7 +94,7 @@ app.controller("searchCompany", function($scope){
 		{
 			"name" : "VMWare",
 			"description" : "The pace of business is melting away rigid structures and requires a new model of IT—one that is fluid, instant and secure. VMware offers that model—Brave New IT, empowering customers to rapidly develop, automatically deliver and safely consume all applications, from traditional to cloud-native, on any device and to manage them through a unified platform.",
-			"link" : "http://cdn.arstechnica.net/wp-content/uploads/2011/11/vmware_headquarters-4eb4083-intro.jpg",
+			"link" : "http://cdn.arstechnica.net/wp-content/uploads/2011/11/vmware_headquarters-4eb4083-intro.jpg" class="thumbnail" height="100" width="100",
 			"facebook" : "https://www.facebook.com/vmware",
 			"linkedin" : "https://www.linkedin.com/company/vmware",
 			"twitter" : "https://twitter.com/VMware?",
@@ -102,7 +103,7 @@ app.controller("searchCompany", function($scope){
 		{
 			"name" : "Oracle",
 			"description" : "At Oracle, we want to help you shift that spending balance. We help you simplify your IT environment so that you can free up money, time, and resources to invest in innovation. We do this by providing hardware and software that’s engineered to work together, in the cloud or on premises.",
-			"link" : "http://news.doddleme.com/wp-content/uploads/2012/09/oracle-building.jpg",
+			"link" : "http://news.doddleme.com/wp-content/uploads/2012/09/oracle-building.jpg" class="thumbnail" height="100" width="100",
 			"facebook" : "https://www.facebook.com/Oracle",
 			"linkedin" : "https://www.linkedin.com/company/oracle",
 			"twitter" : "https://twitter.com/oracle",
@@ -111,7 +112,7 @@ app.controller("searchCompany", function($scope){
 		{
 			"name" : "Salesforce",
 			"description" : "Salesforce.com is a global cloud computing company headquartered in San Francisco, California. Though best known for its customer relationship management (CRM) product, Salesforce has also expanded into commercial applications of social networking through acquisition.",
-			"link" : "https://dz2kgivhhny1y.cloudfront.net/content/uploads/2014/10/salesforce-2.jpg",
+			"link" : "https://dz2kgivhhny1y.cloudfront.net/content/uploads/2014/10/salesforce-2.jpg" class="thumbnail" height="100" width="100",
 			"facebook" : "https://www.facebook.com/salesforce",
 			"linkedin" : "https://www.linkedin.com/company/salesforce",
 			"twitter" : "https://twitter.com/salesforce",
@@ -120,7 +121,7 @@ app.controller("searchCompany", function($scope){
 		{
 			"name" : "Optimizely",
 			"description" : "Optimizely is a firm that makes customer experience optimization software for companies. The Optimizely platform technology provides businesses the ability to conduct A/B testing, multipage, and multivariate testing that may be used for making data-driven decisions.",
-			"link" : "https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e15/11190147_1634590883425805_1433452728_n.jpg",
+			"link" : "https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e15/11190147_1634590883425805_1433452728_n.jpg" class="thumbnail" height="100" width="100",
 			"facebook" : "https://www.facebook.com/optimizely",
 			"linkedin" : "https://www.linkedin.com/company/optimizely",
 			"twitter" : "https://twitter.com/optimizely",
@@ -129,7 +130,7 @@ app.controller("searchCompany", function($scope){
 		{
 			"name" : "Arista Networks",
 			"description" : "Arista Networks is a computer networking company headquartered in Santa Clara, California, USA. The company designs and sells multilayer network switches to deliver software-defined networking (SDN) solutions for large datacenter, cloud computing, high-performance computing and high-frequency trading environments.",
-			"link" : "http://www.hpcwire.com/wp-content/uploads/2014/06/arista.png",
+			"link" : "http://www.hpcwire.com/wp-content/uploads/2014/06/arista.png" class="thumbnail" height="100" width="100",
 			"facebook" : "https://www.facebook.com/arista",
 			"linkedin" : "https://www.linkedin.com/company/arista",
 			"twitter" : "https://twitter.com/arista",
