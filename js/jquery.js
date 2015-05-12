f3d0767056b8a3b909c825b48126d589e11da38f
David Tran <tran.david.619@gmail.com>
2015-05-12 03:03:45 -0700

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -23,3 +23,20 @@ $('#recruiters').ready(function(){
         $(this).removeClass('transition');
     });
   });
+   
+   //Used to enlarge images
+   $('#recruiters').click(function showImage(imgName) {
+                document.getElementById('largeImg').src = imgName;
+                showLargeImagePanel();
+                unselectAll();
+            })
+            $('#recruiters').click(function showLargeImagePanel() {
+                document.getElementById('largeImgPanel').style.visibility = 'visible';
+            })
+            $('#recruiters').click(function unselectAll() {
+                if(document.selection) document.selection.empty();
+                if(window.getSelection) window.getSelection().removeAllRanges();
+            })
+            $('#recruiters').click(function hideMe(obj) {
+                obj.style.visibility = 'hidden';
+            })
